527cc23cf4a8816e5868b7a41c485c5000c66d1c
Dig-Dug <40063087+Dig-Dug@users.noreply.github.com>
2021-03-26 12:40:00 +0100

    Update like_button.js

--- like_button.js
+++ like_button.js
@@ -1,14 +1,20 @@
 'use strict';
 
 const e = React.createElement;
+const rt = React.createElement;
+const name1 = "Josh Perez";
+const element = "Hello, world";
 
 class LikeButton extends React.Component {
   constructor(props) {
+    
     super(props);
     this.state = { liked: false };
   }
 
   render() {
+    
+
     if (this.state.liked) {
       return 'You liked this.'+ 12;
     }
@@ -16,10 +22,37 @@ class LikeButton extends React.Component {
     return e(
       'button',
       { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
+      'Like', element + name1
+    
+      );
+      
+  }
+}
+class u extends React.Component{
+  constructor(props) {
+    
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'Y555555555ou liked this.'+ 12;
+    }
+    return rt(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Shite'
+    
+      );
   }
 }
 
 const domContainer = document.querySelector('#like_button_container');
+
+const doomContainer = document.querySelector('#test');
 ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(rt(u), doomContainer);
+
+
+ReactDOM.render(element, document.getElementById('root'));
